9f4fe22e9aba3130aef24f02a61d907aebf5425f
bunda3d <bunda3@yahoo.com>
2020-10-30 05:19:24 -0500

    Got androidx.fragment.app.FragmentContainerView fixed

--- app/src/main/java/com/krisbunda/gamesmart/MainActivity.kt
+++ app/src/main/java/com/krisbunda/gamesmart/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -36,7 +37,11 @@ class MainActivity : AppCompatActivity() {
         }
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
+        // had to comment this and use "fragment manager" per IDE's suggestion of FragmentContainerView
+        // https://stackoverflow.com/a/59275182
+        // val navController = findNavController(R.id.nav_host_fragment)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
