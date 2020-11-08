cf137f91b673de763d7381a26ce296b592238c62
bunda3d <bunda3@yahoo.com>
2020-11-08 15:03:42 -0600

    add category pages and navigation

--- app/src/main/java/com/krisbunda/gamesmart/CartActivity.kt
+++ app/src/main/java/com/krisbunda/gamesmart/CartActivity.kt
@@ -1,11 +1,19 @@
 package com.krisbunda.gamesmart
 
 import android.os.Bundle
+import android.util.Log.d
+import android.view.MenuItem
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.krisbunda.gamesmart.model.ArtSuppliesFragment
+import com.krisbunda.gamesmart.model.MakerKitsFragment
 import com.krisbunda.gamesmart.model.Product
+import com.krisbunda.gamesmart.model.PuzzlesFragment
+import kotlinx.android.synthetic.main.activity_cart.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 import kotlinx.android.synthetic.main.content_cart.*
 
@@ -15,6 +23,38 @@ class CartActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        nav_view_cart.setNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.navcat_home -> {
+                    d("catnav", "navcat_home was pressed")
+                }
+                R.id.navcat_art_supplies -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.navcat_host_fragment, ArtSuppliesFragment()).commit()
+                    d("catnav", "navcat_art_supplies was pressed")
+                }
+                R.id.navcat_maker_kits -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.navcat_host_fragment, MakerKitsFragment()).commit()
+                    d("catnav", "navcat_maker_kits was pressed")
+                }
+                R.id.navcat_puzzles -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.navcat_host_fragment, PuzzlesFragment()).commit()
+                    d("catnav", "navcat_puzzles was pressed")
+                }
+            }
+            it.isChecked = true
+            drawer_layout_cart.closeDrawers()
+            true
+        }
+
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu_menu)
+
+        }
 
         //initialize the array of product data
         val products = arrayListOf<Product>()
@@ -38,4 +78,13 @@ class CartActivity : AppCompatActivity() {
                 .setAction("Action", null).show()
         }
     }
+
+    //fix this w/better solution later (store hamburger functionality)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        drawer_layout_cart.openDrawer(GravityCompat.START)
+        return true
+        //return super.onOptionsItemSelected(item)
+    }
+
+
 }
---
+++ app/src/main/java/com/krisbunda/gamesmart/model/MakerKitsFragment.kt
@@ -0,0 +1,18 @@
+package com.krisbunda.gamesmart.model
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.krisbunda.gamesmart.R
+
+class MakerKitsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_cat_makerkits, container, false)
+    }
+}
