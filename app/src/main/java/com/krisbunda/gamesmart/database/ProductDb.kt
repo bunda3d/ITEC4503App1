227346b05d137dc8a77569899c23a79433b54450
bunda3d <bunda3@yahoo.com>
2020-11-09 16:47:40 -0600

    strip out db and location srvc

--- app/src/main/java/com/krisbunda/gamesmart/database/ProductDb.kt
+++ app/src/main/java/com/krisbunda/gamesmart/database/ProductDb.kt
@@ -5,10 +5,10 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
-data class ProductDb() {
+data class ProductDb(
     @PrimaryKey(autoGenerate = true) val id: Int?,
 
     @ColumnInfo val title: String?,
 
     @ColumnInfo val price: Double = 0.0
-}
+)
