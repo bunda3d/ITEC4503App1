f5821751d24fe4c90d014adffd25f52e38f86883
bunda3d <bunda3@yahoo.com>
2020-11-11 02:41:43 -0600

    cannot add photourl to DB entity and get cards to populate

--- app/src/main/java/com/krisbunda/gamesmart/model/ShopAdminFragment.kt
+++ app/src/main/java/com/krisbunda/gamesmart/model/ShopAdminFragment.kt
@@ -6,13 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.room.Dao
 import androidx.room.Room
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.gson.Gson
-import com.krisbunda.gamesmart.ProductsAdapter
 import com.krisbunda.gamesmart.R
 import com.krisbunda.gamesmart.database.AppDatabase
 import com.krisbunda.gamesmart.database.ProductData
@@ -21,7 +15,6 @@ import kotlinx.android.synthetic.main.fragment_shop.view.*
 import kotlinx.android.synthetic.main.fragment_shop_admin.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
-import java.net.URL
 
 class ShopAdminFragment : Fragment() {
     override fun onCreateView(
@@ -53,10 +46,12 @@ class ShopAdminFragment : Fragment() {
                     AppDatabase::class.java, "ProdDataDb"
                 ).build()
 
+
                 db.productDao().insertAll(ProductData(
                     null,
                     title.toString(),
                     //"https://via.placeholder.com/300/BB86FC/FFFFFF/?text=GameSmart",
+                    //it.photoUrl,
                     "ipsum in voluptate fugiat irure pariatur mollit non deserunt reprehenderit dolore id officia sit",
                     65.00,
                     65000
