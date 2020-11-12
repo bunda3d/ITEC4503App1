d73ab95ca45b3f7327a89c46b89700ba569cdb82
bunda3d <bunda3@yahoo.com>
2020-11-11 23:38:02 -0600

    add cart checkout page

--- app/src/main/java/com/krisbunda/gamesmart/cart/CartStatusActivity.kt
+++ app/src/main/java/com/krisbunda/gamesmart/cart/CartStatusActivity.kt
@@ -4,11 +4,10 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.krisbunda.gamesmart.R
-import com.krisbunda.gamesmart.R.*
 
 class CartStatusActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        setContentView(layout.activity_cart_status)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart_status)
     }
 }
